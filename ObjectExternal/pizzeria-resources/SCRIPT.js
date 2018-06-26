6e1e0d92df36d8881de124a0971f8e5eb16cb083
designer (Designer) <>
2018-06-26 21:56:38 +0200

    Updated

--- ObjectExternal/pizzeria-resources/SCRIPT.js
+++ ObjectExternal/pizzeria-resources/SCRIPT.js
@@ -1,4 +1,4 @@
-if (typeof pizzeria === "undefined") pizzeria = (function($) {
+if (typeof pizzeria === 'undefined') pizzeria = (function($) {
 	var app;
 
 	function render(root, banner) {
@@ -7,13 +7,39 @@ if (typeof pizzeria === "undefined") pizzeria = (function($) {
 		piz.toFixed = function() { return function(n, r) { return parseFloat(r(n)).toFixed(2); } }; // Mustache rendering for decimal
 		piz.bannerURL = banner;
 		piz.search(function() {
-			$("#pizzeria").html(Mustache.render($("#pizzeria-template").html(), piz)).find("img").click(function() {
-				var item = piz.getItem($(this).data("id")); // Get list item from row ID
-				if (item.pzaPizVideo) {
-					var video = $("<iframe/>", { width: 560, height: 315, src: item.pzaPizVideo, frameborder: "0", allow: "autoplay; encrypted-media", allowfullscreen: true });
-					bootbox.alert({ title: item.pzaPizName, message: video });
-				}
+			var p = $('#pizzeria').html(Mustache.render($('#pizzeria-template').html(), piz));
+			p.find('img').click(function() {
+				var pizza = piz.getItem($(this).data('id')); // Get list item from row ID
+				if (pizza.pzaPizVideo)
+					bootbox.alert({
+						title: pizza.pzaPizName,
+						message: Mustache.render($('#pizzeria-video-template').html(), pizza)
+					});
 			});
+			p.find('button').click(function() {
+				var pizza = piz.getItem($(this).data('id')); // Get list item from row ID
+				bootbox.confirm({
+					title: pizza.pzaPizName,
+					message: Mustache.render($('#pizzeria-order-template').html(), pizza),
+					callback: function(res) {
+						if (res) {
+							var ord = app.getBusinessObject('PzaOrder');
+							ord.item.pzaOrdPizId = pizza.row_id;
+							ord.item.pzaOrdName = $('#pizzeria-name').val();
+							ord.item.pzaOrdAddress = $('#pizzeria-address').val();
+							ord.item.pzaOrdPhone = $('#pizzeria-phone').val();
+							ord.item.pzaOrdEmail = $('#pizzeria-email').val();
+							ord.create(function() {
+								bootbox.alert({ message: 'Order placed! Thank you.' });
+							});
+						}
+					},
+					buttons: {
+						confirm: { label: 'Order', className: 'btn-success' },
+						cancel: { label: 'Cancel', className: 'btn-danger' }
+					}
+				});
+			})
 		}, {}, { inlineDocs: true });
 	}
 
