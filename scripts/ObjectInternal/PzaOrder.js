ac1b9d2a7852d9ae6c655c0b92c800f83a224e16
designer (Designer) <no-reply@simplicite.fr>
2019-06-17 10:17:18 +0200

    Updated

--- scripts/ObjectInternal/PzaOrder.js
+++ scripts/ObjectInternal/PzaOrder.js
@@ -1,7 +1,11 @@
 PzaOrder.preCreate = function() {
-	var origin = this.getGrant().getParameter("PZA_ORIGIN_COORDINATES");
-	var maxDistance = this.getGrant().getIntParameter("PZA_MAX_DISTANCE");
-	var distance = GMapTool.distance(origin, this.getFieldValue("pzaOrdCoordinates"), GMapTool.MODE_DRIVING, GMapTool.UNITS_IMPERIAL);
-	if (distance > maxDistance)
-		return Message.formatSimpleError("PZA_ERR_DISTANCE:" + distance + " miles > " + maxDistance + " miles");
+	try {
+		var origin = this.getGrant().getParameter("PZA_ORIGIN_COORDINATES");
+		var maxDistance = this.getGrant().getIntParameter("PZA_MAX_DISTANCE");
+		var distance = new GMapTool(this.getGrant()).distance(origin, this.getFieldValue("pzaOrdCoordinates"), GMapTool.MODE_DRIVING, GMapTool.UNITS_IMPERIAL);
+		if (distance > maxDistance)
+			return Message.formatSimpleError("PZA_ERR_DISTANCE:" + distance + " miles > " + maxDistance + " miles");
+	} catch(e) {
+		return Message.formatSimpleWarning(e.message);
+	}
 };
