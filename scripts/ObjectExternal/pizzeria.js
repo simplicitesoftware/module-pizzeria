e43f00d752ef53536628e43196dc72118019f0b3
designer (Designer) <no-reply@simplicite.fr>
2019-07-31 17:46:16 +0200

    Google Maps URL

--- scripts/ObjectExternal/pizzeria.js
+++ scripts/ObjectExternal/pizzeria.js
@@ -3,6 +3,7 @@ pizzeria.display = function(params) {
 	this.setDecoration(false);
 
 	var banner = HTMLTool.getResourceImageURL(this, "BANNER");
+	var gmapJS = new GMapTool(g).getJSURL();
 	var render = "pizzeria.render('" + Globals.getContextPath() + "', " + this.isPublic() + ", '" + banner + "')";
 
 	if (this.isPublic()) {
@@ -11,7 +12,7 @@ pizzeria.display = function(params) {
 		wp.appendAjax();
 		wp.appendMustache();
 		wp.appendJSIncludes(HTMLTool.bootboxJS());
-		wp.appendJSInclude("https://maps.googleapis.com/maps/api/js?key=" + g.getParameter("GOOGLE_API_KEY") + "&libraries=places&language=en-US");
+		wp.appendJSInclude(gmapJS);
 		wp.appendCSSInclude(HTMLTool.getResourceCSSURL(this, "STYLES"));
 		wp.appendJSInclude(HTMLTool.getResourceJSURL(this, "SCRIPT"));
 		wp.append(HTMLTool.getResourceHTMLContent(this, "HTML"));
@@ -20,6 +21,7 @@ pizzeria.display = function(params) {
 	} else {
 		this.addMustache();
 		this.addExtraJS(HTMLTool.bootboxJS());
+		this.addExtraJS(gmapJS);
 		return this.javascript(render);
 	}
 };
